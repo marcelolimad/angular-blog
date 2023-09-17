850da3a4aba93a388e994c6b0bcd9a52b3ad5fb2
marcelolimad <marcelolimad@gmail.com>
2023-09-16 21:34:18 -0300

    components createds with styles

---
+++ src/app/components/smart-card/smart-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-smart-card',
+  templateUrl: './smart-card.component.html',
+  styleUrls: ['./smart-card.component.css']
+})
+export class SmartCardComponent {
+
+}
